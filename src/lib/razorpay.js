5a2c20c31d8bae025e7bf64cf37fc1e9ddb302a4
satyamprabhat296 <satyam7015@gmail.com>
2026-04-12 23:26:21 +0530

    Update razorpay.js

--- src/lib/razorpay.js
+++ src/lib/razorpay.js
@@ -1,8 +1,12 @@
 import Razorpay from "razorpay";
 
-const razorpay = new Razorpay({
-  key_id: process.env.RAZORPAY_KEY_ID,
-  key_secret: process.env.RAZORPAY_KEY_SECRET,
-});
+export const getRazorpayInstance = () => {
+  if (!process.env.RAZORPAY_KEY_ID || !process.env.RAZORPAY_KEY_SECRET) {
+    throw new Error("Razorpay keys missing in environment variables");
+  }
 
-export default razorpay;
+  return new Razorpay({
+    key_id: process.env.RAZORPAY_KEY_ID,
+    key_secret: process.env.RAZORPAY_KEY_SECRET,
+  });
+};
